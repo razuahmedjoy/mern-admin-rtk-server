11d60238ab0f71fbe18640fabb2d7c8276497072
razuahmedjoy <rjjoybzs420@gmail.com>
2023-02-04 02:31:53 +0600

    get customers api done

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (req, res) => {
     try{
@@ -18,5 +19,17 @@ export const getProducts = async (req, res) => {
 
     }catch(e){
         res.status(404).json({message:e.message});
+    }
+}
+
+export const getCustomers = async (req,res)=>{
+
+    try{
+        const customers = await User.find({role:"user"}).select("-password");
+        res.status(200).json(customers);
+
+    }
+    catch(e){
+        res.status(404).json({message:e.message})
     }
 }
--- routes/client.route.js
+++ routes/client.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import {getProducts} from "../controllers/client.controller.js";
+import {getCustomers, getProducts} from "../controllers/client.controller.js";
 
 router.get("/products",getProducts)
+router.get("/customers",getCustomers)
 
 export default router;
